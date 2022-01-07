bab8fc06762a46f2a7d5dfcf3f96aa8a76767c72
TT-ReBORN <thomas.tibitanzl@gmx.net>
2022-01-08 00:53:54 +0100

    Show artist and label logos only when needed, Part 2 - 31329ec

--- profile/georgia-reborn/js/georgia-reborn-main.js
+++ profile/georgia-reborn/js/georgia-reborn-main.js
@@ -848,7 +848,7 @@ function draw_ui(gr) {
 		if (timings.showExtraDrawTiming) drawTextGrid.Print();
 	} /* if (!displayPlaylist && !displayLibrary) */
 
-	if ((fb.IsPlaying && (!displayPlaylist && pref.layout_mode === 'default_mode') && !displayLibrary) || (!albumart && !cdart && noArtwork && !displayPlaylist && pref.layout_mode === 'default_mode')) {
+	if ((fb.IsPlaying && (!displayPlaylist && pref.layout_mode === 'default_mode') && !displayLibrary) || (!albumart && !cdart && noArtwork && !displayPlaylist && !displayLibrary && pref.layout_mode === 'default_mode')) {
 		let drawLogos = null;
 		timings.showExtraDrawTiming && (drawLogos = fb.CreateProfiler('on_paint -> logos/labels'));
 		// BAND LOGO drawing code
