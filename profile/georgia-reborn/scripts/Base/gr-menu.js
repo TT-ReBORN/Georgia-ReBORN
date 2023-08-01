f152383099345f949d84ba0750ec2a60d9a9fd8b
TT-ReBORN <thomas.tibitanzl@gmx.net>
2023-08-01 18:02:16 +0200

    Disabled theme brightness when using harmonic mode

--- profile/georgia-reborn/scripts/Base/gr-menu.js
+++ profile/georgia-reborn/scripts/Base/gr-menu.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-RC1                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2023-07-29                                          * //
+// * Last change:    2023-08-01                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -882,7 +882,7 @@ function brightnessOptions(menu) {
 		if (!pref.themeSandbox) pref.savedThemeBrightness = pref.themeBrightness = percent; else pref.themeBrightness = percent;
 		initThemeFull = true;
 		initTheme();
-	});
+	}, pref.presetSelectMode === 'harmonic');
 }
 
 
