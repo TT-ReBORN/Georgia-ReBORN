d93d008c451f9e0c973b9b605340868c615a89a9
TT-ReBORN <thomas.tibitanzl@gmx.net>
2023-09-20 14:43:29 +0200

    Fixed rare art cache crash on startup

--- profile/georgia-reborn/scripts/Base/gr-callbacks.js
+++ profile/georgia-reborn/scripts/Base/gr-callbacks.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-RC1                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2023-09-02                                          * //
+// * Last change:    2023-09-20                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -310,7 +310,7 @@ function on_size() {
 		rescalePlaylist(true);
 		initPlaylist();
 		volumeBtn = new VolumeBtn();
-		artCache.clear();
+		artCache && artCache.clear();
 		artCache = new ArtCache(15);
 		sizeInitialized = true;
 		if (str.timeline) {
