bb344797393c7409e1dc3d5c82875f68fbdc68ce
TT-ReBORN <thomas.tibitanzl@gmx.net>
2023-09-22 22:44:14 +0200

    Added missing jsdoc in gr-lyrics.js

--- profile/georgia-reborn/scripts/Base/gr-lyrics.js
+++ profile/georgia-reborn/scripts/Base/gr-lyrics.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-RC1                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2023-09-20                                          * //
+// * Last change:    2023-09-22                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -192,6 +192,9 @@ class Lyrics {
 		}, 60000);
 	}
 
+	/**
+	 * Cycles through the next lyric source in the search results, used for ESLyric's "Next lyric" feature.
+	 */
 	nextLyrics() {
 		const nextSrc = (meta) => {
 			fb.RunMainMenuCommand('View/ESLyric/Panels/Delete lyric');
