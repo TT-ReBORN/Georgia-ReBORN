TT-ReBORN/Georgia-ReBORN

commit 70e0a08401721dbe814d9efcb0edd72c8f0a8e2b

Refactored `forEach` to `for...of` loops, JSDoc cleanup

profile/georgia-reborn/scripts/Base/gr-helpers.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2024-01-13                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -414,13 +414,13 @@ function _CreateFolder(folder) {
 	if (!IsFolder(folder)) {
 		if (folder.startsWith('.\\')) { folder = fb.FoobarPath + folder.replace('.\\', ''); }
 		const subFolders = folder.split('\\').map((_, i, arr) => i ? arr.slice(0, i).reduce((path, name) => `${path}\\${name}`) : _);
-		subFolders.forEach((path) => {
+		for (const path of subFolders) {
 			try {
 				fso.CreateFolder(path);
 			} catch (e) {
 				return false;
 			}
-		});
+		}
 		return IsFolder(folder);
 	}
 	return false;
@@ -1014,13 +1014,13 @@ function CalcImgBrightness(image) {
 		let bTot = 0;
 		let freqTot = 0;
 
-		colorSchemeArray.forEach(v => {
+		for (const v of colorSchemeArray) {
 			const col = ToRGB(v.col);
 			rTot += col[0] ** 2 * v.freq;
 			gTot += col[1] ** 2 * v.freq;
 			bTot += col[2] ** 2 * v.freq;
 			freqTot += v.freq;
-		});
+		}
 
 		const avgCol =
 			Math.round([
@@ -1189,7 +1189,6 @@ class ImageSize {
 	 * @param {number} y The y-coordinate of the image.
 	 * @param {number} w The width of the image.
 	 * @param {number} h The height of the image.
-	 * @class
 	 */
 	constructor(x, y, w, h) {
 		this.x = x;
@@ -1478,12 +1477,14 @@ function TestFont(fontName) {
  */
 function CalcGridMaxTextWidth(gr, gridArray, font) {
 	let maxWidth = 0;
-	gridArray && gridArray.forEach((el) => {
-		const width = Math.ceil(gr.MeasureString(el.label, font, 0, 0, ww, wh).Width) + 1;
-		if (width > maxWidth) {
-			maxWidth = width;
+	if (gridArray) {
+		for (const el of gridArray) {
+			const width = Math.ceil(gr.MeasureString(el.label, font, 0, 0, ww, wh).Width) + 1;
+			if (width > maxWidth) {
+				maxWidth = width;
+			}
 		}
-	});
+	}
 	return maxWidth;
 }
 
@@ -1632,8 +1633,8 @@ function DeepAssign(options = { nonEnum: false, symbols: false, descriptors: fal
 	 * @returns {void} Void if all sources are valid Objects.
 	 */
 	return function deepAssignWithOptions (target, ...sources) {
-		sources.forEach((source) => {
-			if (!IsDeepObject(source) || !IsDeepObject(target)) { return; }
+		for (const source of sources) {
+			if (!IsDeepObject(source) || !IsDeepObject(target)) { continue; }
 			// Copy source's own properties into target's own properties
 			const copyProperty = (property) => {
 				const descriptor = Object.getOwnPropertyDescriptor(source, property);
@@ -1652,10 +1653,14 @@ function DeepAssign(options = { nonEnum: false, symbols: false, descriptors: fal
 				}
 			};
 			// Copy string-keyed properties
-			Object.getOwnPropertyNames(source).forEach(copyProperty);
+			for (const property of Object.getOwnPropertyNames(source)) {
+				copyProperty(property);
+			}
 			// default: omit symbol-keyed properties
 			if (options.symbols) {
-				Object.getOwnPropertySymbols(source).forEach(copyProperty);
+				for (const symbol of Object.getOwnPropertySymbols(source)) {
+					copyProperty(symbol);
+				}
 			}
 			// default: omit prototype's own properties
 			if (options.proto) {
@@ -1665,7 +1670,7 @@ function DeepAssign(options = { nonEnum: false, symbols: false, descriptors: fal
 					Object.getPrototypeOf(source)
 				);
 			}
-		});
+		}
 		return target;
 	}
 }

profile/georgia-reborn/scripts/Base/gr-async-loader.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2024-01-09                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -145,7 +145,7 @@ async function includeFiles(fileList, startTime) {
 		}
 		return;
 	}
-	fileList.forEach(filePath => include(`${basePath}${filePath}`));
+	for (const filePath of fileList) include(`${basePath}${filePath}`)
 }
 
 

profile/georgia-reborn/scripts/Base/gr-buttons.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2024-01-08                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -66,7 +66,6 @@ class Button {
 	 * @param {GdiBitmap[]} img The image that will be displayed for the button.
 	 * @param {string} tip The tooltip text for the button.
 	 * @param {boolean} isEnabled A callback function that determines whether the button is enabled or disabled.
-	 * @class
 	 */
 	constructor(x, y, w, h, id, img, tip = undefined, isEnabled = undefined) {
 		this.x = x;

profile/georgia-reborn/scripts/Base/gr-common.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2024-01-13                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -1229,7 +1229,6 @@ const qwr_supress_mouse_move = qwr_utils.MouseMoveSuppress();
  * Provides a way to manage key action events, allowing you to register
  * and invoke actions based on key inputs. It maintains an internal registry
  * of key actions, which can be triggered by invoking the registered keys.
- * @class
  */
 class KeyActionHandler {
 	/**
@@ -1286,7 +1285,6 @@ class KeyActionHandler {
 //////////////////////////
 /**
  * Creates an object with a name and a value, and provides methods to get and set the value while also storing.
- * @class
  */
 class PanelProperty {
 	/**
@@ -1323,7 +1321,6 @@ class PanelProperty {
 
 /**
  * Allows to add and manage SMP properties with their names and default values.
- * @class
  */
 class PanelProperties {
 	constructor() {
@@ -1338,10 +1335,10 @@ class PanelProperties {
 	 * @param {Object} properties Each item in array is an object of the following type { string, [string, any] }.
 	 */
 	add_properties(properties) {
-		Object.keys(properties).forEach(key => {
+		for (const key of Object.keys(properties)) {
 			this.validate_property_item(properties[key], key);
 			this.add_property_item(properties[key], key);
-		});
+		}
 	}
 
 	/**

profile/georgia-reborn/scripts/Base/gr-configuration.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2024-01-01                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -38,7 +38,6 @@ class ConfigurationObjectSchema {
 	 * @param {string} container The type of container for the object. Should be of ConfigurationObjectType.
 	 * @param {Array<FieldDefinition>=} fields The fields for each entry in the object. If undefined, uses key/value pairs for objects, or comma separated values for arrays.
 	 * @param {string=} comment Adds a '//' field as first entry in the object. Used for explaining things to the user.
-	 * @class
 	 */
 	constructor(name, container, fields = undefined, comment = undefined) {
 		this.name = name;
@@ -62,7 +61,6 @@ class Configuration {
 	/**
 	 * Instantiates a Configuration object and specifies a file to read from.
 	 * @param {string} configurationPath The path to the config file.
-	 * @class
 	 */
 	constructor(configurationPath) {
 		this.path = configurationPath;
@@ -192,7 +190,7 @@ class Configuration {
 		p.WriteLine('/////////////////////////////////////////////////////////////////////////////');
 		p.WriteLine('');
 
-		this._configuration.forEach((conf, i) => {
+		for (const conf of this._configuration) {
 			p.WriteLine('');
 			const container = conf.definition.container === ConfigurationObjectType.Array ? '[' : '{';
 			p.WriteLine(`\t"${conf.definition.name}": ${container}`);
@@ -215,39 +213,39 @@ class Configuration {
 
 			if (conf.definition.fields) {
 				// Array of fields
-				for (let i = 0; i < conf.values.length; i++) {
+				for (const value of conf.values) {
 					let entry = '';
 					if (conf.definition.container === ConfigurationObjectType.Array) {
-						conf.definition.fields.forEach(field => {
-							if (!field.optional || conf.values[i][field.name]) {
-								const quotes = typeof conf.values[i][field.name] === 'string' ? '"' : '';
-								entry += `, "${field.name}": ${quotes}${conf.values[i][field.name]}${quotes}`;
+						for (const field of conf.definition.fields) {
+							if (!field.optional || value[field.name]) {
+								const quotes = typeof value[field.name] === 'string' ? '"' : '';
+								entry += `, "${field.name}": ${quotes}${value[field.name]}${quotes}`;
 							}
-						});
+						}
 						entry = `{${entry.substr(1)} }`;
 					}
-					const comment = conf.values[i].comment ? ` // ${conf.values[i].comment}` : '';
-					p.WriteLine(`\t\t${entry}${i < conf.values.length - 1 ? ',' : ''}${comment}`);
+					const comment = value.comment ? ` // ${value.comment}` : '';
+					p.WriteLine(`\t\t${entry}${value !== conf.values[conf.values.length - 1] ? ',' : ''}${comment}`);
 				}
 			}
 			else if (conf.definition.container === ConfigurationObjectType.Array) {
 				// Array of comma separated entries
-				conf.values.forEach((val, i) => {
-					p.WriteLine(`\t\t"${val.replace(/\\/g, '\\\\')}"${i < conf.values.length - 1 ? ',' : ''}`);
-				});
+				for (const val of conf.values) {
+					p.WriteLine(`\t\t"${val.replace(/\\/g, '\\\\')}"${val !== conf.values[conf.values.length - 1] ? ',' : ''}`);
+				}
 			}
 			else {
 				// Object with key/value pairs
 				const keys = Object.keys(conf.values);
-				keys.forEach((key, i) => {
+				for (const key of keys) {
 					const comment = conf.comments[key] ? ` // ${conf.comments[key]}` : '';
 					const quotes = typeof conf.values[key] === 'string' ? '"' : '';
-					p.WriteLine(`\t\t"${key}": ${quotes}${conf.values[key]}${quotes}${i < keys.length - 1 ? ',' : ''}${comment}`);
-				});
+					p.WriteLine(`\t\t"${key}": ${quotes}${conf.values[key]}${quotes}${key !== keys[keys.length - 1] ? ',' : ''}${comment}`);
+				}
 			}
 			const closeContainer = conf.definition.container === ConfigurationObjectType.Array ? ']' : '}';
-			p.WriteLine(`\t${closeContainer}${i < this._configuration.length - 1 ? ',' : ''}`);
-		});
+			p.WriteLine(`\t${closeContainer}${conf !== this._configuration[this._configuration.length - 1] ? ',' : ''}`);
+		}
 
 		p.WriteLine('}');
 		p.Close();
@@ -283,7 +281,6 @@ class ThemeSetting {
 	/**
 	 * @param {string} name The name of the setting.
 	 * @param {*} settingVal The value of the setting.
-	 * @class
 	 */
 	constructor(name, settingVal) {
 		/** @const {string} */
@@ -319,7 +316,6 @@ class ThemeSettings {
 	 * @param {Configuration} config The config.
 	 * @param {String} objName The name of the config.
 	 * @param {ConfigurationObject} properties The properties to add to the config.
-	 * @class
 	 */
 	constructor(config, objName, properties = undefined) {
 		/** @protected */
@@ -338,10 +334,10 @@ class ThemeSettings {
 	 * @param {ConfigurationObject|*} properties Each item in array is an array of objects
 	 */
 	add_properties(properties) {
-		Object.keys(properties).forEach(key => {
+		for (const key of Object.keys(properties)) {
 			this.validate_config_item(properties[key], key);
 			this.add_config_item(properties[key], key);
-		});
+		}
 	}
 
 	/**

profile/georgia-reborn/scripts/Base/gr-display.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2023-12-18                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -60,12 +60,8 @@ function SCALE(val) {
 /////////////////
 /**
  * Manages and sets the window size based on active display resolution, layout setting and player size.
- * @type {Object} Sets the windows size via UIHacks.
  */
 class Display {
-	/**
-	 * @class
-	 */
 	constructor() {
 		/**
 		 * Saves last used active window width and height.

profile/georgia-reborn/scripts/Base/gr-lyrics.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN         * //
 // * Version:        3.0-DEV                                             * //
 // * Dev. started:   2017-12-22                                          * //
-// * Last change:    2024-01-10                                          * //
+// * Last change:    2024-01-15                                          * //
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -33,9 +33,6 @@ let lyricType;
  * Loads and displays lyrics on album art in the Lyrics panel from the cache directory or embedded files.
  */
 class Lyrics {
-	/**
-	 * @class
-	 */
 	constructor() {
 		/** @type {<Array<string>>} */
 		this.lyr = [];
@@ -137,12 +134,12 @@ class Lyrics {
 		const stripReservedChars = (filename) => filename.replace(/[<>:"/\\|?*]/g, '_');
 		const lyricPaths = pref.customLyricsDir ? globals.customLyricsDir : globals.lyr_path;
 
-		lyricPaths.forEach(path => {
+		for (const path of lyricPaths) {
 			tpath.push($($Escape(path)));
-		});
-		globals.lyricFilenamePatterns.forEach(filename => {
+		}
+		for (const filename of globals.lyricFilenamePatterns) {
 			tfilename.push(stripReservedChars($(filename)));
-		});
+		}
 
 		for (let i = 0; i < tpath.length && !foundLyrics; i++) {
 			for (const file of tfilename) {
@@ -322,7 +319,7 @@ class Lyrics {
 			case this.type.unsynced: {
 				this.formatLyrics(this.parseUnsyncedLyrics(lyr, this.type.none));
 				const ratio = isStreaming ? 2000 : this.trackLength / this.lyrics.length * 1000;
-				this.lyrics.forEach((line, i) => { line.timestamp = ratio * i });
+				for (const [i, line] of this.lyrics.entries()) line.timestamp = ratio * i;
 				break;
 			}
 		}
@@ -339,16 +336,16 @@ class Lyrics {
 	parseSyncLyrics(lyr, isNone) {
 		const lyrics = [];
 		if (isNone) lyrics.push({ timestamp: 0, content: lyr[0] });
-		lyr.forEach(line => {
+		for (const line of lyr) {
 			const content = this.tidy(line);
 			const matches = line.match(this.leadingTimestamps);
 			if (matches) {
 				const all = matches[0].split('][');
-				all.forEach(m => {
+				for (const m of all) {
 					lyrics.push({ timestamp: this.getMilliseconds(m), content });
-				});
+				}
 			}
-		});
+		}
 		return lyrics.sort((a, b) => a.timestamp - b.timestamp);
 	}
 
@@ -361,9 +358,9 @@ class Lyrics {
 	parseUnsyncedLyrics(lyr, isNone) {
 		const lyrics = [];
 		if (isNone) lyrics.push({ timestamp: 0, content: lyr[0] });
-		lyr.forEach(line => {
+		for (const line of lyr) {
 			lyrics.push({ timestamp: 0, content: this.tidy(line) });
-		});
+		}
 		return lyrics;
 	}
 
@@ -394,12 +391,12 @@ class Lyrics {
 		}
 		this.maxLyrWidth = Math.min(this.maxLyrWidth + 40, this.w);
 		const incr = Math.min(500, this.durationScroll);
-		this.lyrics.forEach((v, i) => {
+		for (const [i, v] of this.lyrics.entries()) {
 			const t1 = this.getTimestamp(i - 1);
 			const t2 = this.getTimestamp(i);
 			const t3 = this.getTimestamp(i + 1);
 			if (!v.content && t3 && t2 && t1 && t3 - t2 < incr) v.timestamp = Math.max((t2 - t1) / 2 + t1, t2 - incr);
-		});
+		}
 		this.repaintRect();
 	}
 
@@ -445,13 +442,13 @@ class Lyrics {
 
 		gr.SetTextRenderingHint(5);
 
-		this.lyrics.forEach((lyric, i) => {
+		for (const [i, lyric] of this.lyrics.entries()) {
 			const font = lyric.highlight && pref.lyricsLargerCurrentSync ? ft.lyricsHighlight : ft.lyrics;
 			const lyric_y = this.lineHeight * i;
 			const line_y = Math.round(y - top + lyric_y - ((this.stringSearching || this.stringNotFound) ? SCALE(24) : 0));
 			const bottomLine = line_y > this.bot + this.lineHeight * (pref.lyricsFadeScroll ? 2 : 3);
 
-			if (!this.displayLyrics(lyric_y, top)) return;
+			if (!this.displayLyrics(lyric_y, top)) continue;
 
 			if (this.shadowEffect && line_y >= this.top && !bottomLine) { // * Do not show drop shadow at top and bottom
 				if (this.dropNegativeShadowLevel) {
@@ -464,7 +461,7 @@ class Lyrics {
 
 			color = line_y >= this.top ? lyric.highlight ? blendIn : i === this.locus - 1 ? blendOut : bottomLine ? fadeBot : col.lyricsNormal : fadeTop;
 			gr.DrawString(lyric.content, font, color, this.x, line_y, this.w + 1, this.lineHeight + 1, this.alignCenter);
-		});
+		}
 
 		if (this.showOffset) {
 			this.offsetW = gr.CalcTextWidth(`Offset: ${this.userOffset / 1000}s`, ft.notification) + this.lineHeight;
@@ -513,7 +510,7 @@ class Lyrics {
 	 * Removes highlight from all lines in the lyric array.
 	 */
 	clearHighlight() {
-		this.lyrics.forEach(v => { v.highlight = false });
+		for (const v of this.lyrics) { v.highlight = false }
 	}
 
 	/**
@@ -617,7 +614,7 @@ class Lyrics {
 	 */
 	setHighlight() {
 		const { id } = this.lyrics[this.locus];
-		if (this.type.synced) this.lyrics.forEach(v => { if (v.id === id) v.highlight = true; });
+		if (this.type.synced) for (const v of this.lyrics) { if (v.id === id) v.highlight = true; }
 	}
 
 	/**

commit 718d736575ea2ebf67edb249c4049647bdf397ea

Fixed _CreateFolder helper regression from 70e0a08 - Prevented data creation for waveform bar, playlist statistics when cache folders did not exist...

profile/georgia-reborn/scripts/Base/gr-helpers.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN             * //
 // * Version:        3.0-DEV                                                 * //
 // * Dev. started:   22-12-2017                                              * //
-// * Last change:    17-04-2024                                              * //
+// * Last change:    19-04-2024                                              * //
 /////////////////////////////////////////////////////////////////////////////////
 
 
@@ -401,20 +401,33 @@ function CreateFolder(folder, is_recursive) {
  * @returns {boolean} True if the folder was created.
  */
 function _CreateFolder(folder) {
-	if (!folder.length) { return false; }
+	if (!folder.length) {
+		return false;
+	}
+
 	if (!IsFolder(folder)) {
-		if (folder.startsWith('.\\')) { folder = fb.FoobarPath + folder.replace('.\\', ''); }
-		const subFolders = folder.split('\\').map((_, i, arr) => i ? arr.slice(0, i).reduce((path, name) => `${path}\\${name}`) : _);
+		if (folder.startsWith('.\\')) {
+			folder = fb.FoobarPath + folder.replace('.\\', '');
+		}
+
+		const subFolders = folder.split('\\').map((_, i, arr) => i ? arr.slice(0, i + 1).reduce((path, name) => `${path}\\${name}`) : arr[0]);
+		let created = true;
+
 		for (const path of subFolders) {
-			try {
-				fso.CreateFolder(path);
-			} catch (e) {
-				return false;
+			if (!IsFolder(path)) {
+				try {
+					fso.CreateFolder(path);
+				} catch (e) {
+					created = false;
+					break;
+				}
 			}
 		}
-		return IsFolder(folder);
+
+		return created && IsFolder(folder);
 	}
-	return false;
+
+	return true;
 }
 
 