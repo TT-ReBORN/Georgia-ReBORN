TT-ReBORN/Georgia-ReBORN

commit 14ee4273024a51e8e4add9d1dd83c3bfffd70e87

- Improved initialization on startup ( test phase )   * Changed the `gr-callbacks.js` script load order to be the last in the chain   * Improved initialization robustness to ensure that class instance properties are fully loaded for the main callbacks   * Removed class instance property safeguards in drag-and-drop main callbacks   * Fixed cosmetic logo positioning on startup when changing the script loader behavior to synchronous loading

profile/georgia-reborn/scripts/Base/gr-callbacks.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN             * //
 // * Version:        3.0-RC3                                                 * //
 // * Dev. started:   22-12-2017                                              * //
-// * Last change:    27-10-2024                                              * //
+// * Last change:    30-10-2024                                              * //
 /////////////////////////////////////////////////////////////////////////////////
 
 
@@ -198,7 +198,7 @@ function on_char(code) {
  * @param {number} mask - The mouse mask.
  */
 function on_drag_enter(action, x, y, mask) {
-	if (grm.ui && (grm.ui.displayPlaylist || grm.ui.displayPlaylistArtwork)) {
+	if (grm.ui.displayPlaylist || grm.ui.displayPlaylistArtwork) {
 		CallLog('Playlist => on_drag_enter');
 		pl.call.on_drag_enter(action, x, y, mask);
 	}
@@ -218,7 +218,7 @@ function on_drag_enter(action, x, y, mask) {
  * @param {number} mask - The mouse mask.
  */
 function on_drag_over(action, x, y, mask) {
-	if (grm.ui && (grm.ui.displayPlaylist || grm.ui.displayPlaylistArtwork)) {
+	if (grm.ui.displayPlaylist || grm.ui.displayPlaylistArtwork) {
 		CallLog('Playlist => on_drag_over');
 		pl.call.on_drag_over(action, x, y, mask);
 	}
@@ -234,7 +234,7 @@ function on_drag_over(action, x, y, mask) {
  * @global
  */
 function on_drag_leave() {
-	if (grm.ui && (grm.ui.displayPlaylist && !grm.ui.displayLibrary || grm.ui.displayPlaylistArtwork || grm.ui.displayLibrarySplit(true))) {
+	if (grm.ui.displayPlaylist && !grm.ui.displayLibrary || grm.ui.displayPlaylistArtwork || grm.ui.displayLibrarySplit(true)) {
 		CallLog('Playlist => on_drag_leave');
 		pl.call.on_drag_leave();
 	}
@@ -254,7 +254,7 @@ function on_drag_leave() {
  * @param {number} mask - The mouse mask.
  */
 function on_drag_drop(action, x, y, mask) {
-	if (grm.ui && (grm.ui.displayPlaylist && !grm.ui.displayLibrary || grm.ui.displayPlaylistArtwork || grm.ui.displayLibrarySplit(true))) {
+	if (grm.ui.displayPlaylist && !grm.ui.displayLibrary || grm.ui.displayPlaylistArtwork || grm.ui.displayLibrarySplit(true)) {
 		CallLog('Playlist => on_drag_drop');
 		pl.call.on_drag_drop(action, x, y, mask);
 	}

profile/georgia-reborn/scripts/Base/gr-main.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN             * //
 // * Version:        3.0-RC3                                                 * //
 // * Dev. started:   22-12-2017                                              * //
-// * Last change:    29-10-2024                                              * //
+// * Last change:    30-10-2024                                              * //
 /////////////////////////////////////////////////////////////////////////////////
 
 
@@ -1822,6 +1822,8 @@ class MainUI {
 
 		// * Init state
 		if (fb.IsPlaying && fb.GetNowPlaying()) {
+			// Wait for on_playback_new_track to be loaded in gr-callbacks.js
+			await MakeAsync(() => {}, () => typeof on_playback_new_track !== 'undefined');
 			on_playback_new_track(fb.GetNowPlaying());
 		}
 		plman.SetActivePlaylistContext();
@@ -2193,7 +2195,6 @@ class MainUI {
 		await this.initMain();
 		await grm.settings.setThemeSettings(false, false, true);
 		await grm.display.autoDetectRes();
-		await this.initMain();
 
 		grSet.systemFirstLaunch = false;
 	}

profile/georgia-reborn/scripts/Base/gr-async-loader.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN             * //
 // * Version:        3.0-RC3                                                 * //
 // * Dev. started:   22-12-2017                                              * //
-// * Last change:    10-10-2024                                              * //
+// * Last change:    30-10-2024                                              * //
 /////////////////////////////////////////////////////////////////////////////////
 
 
@@ -112,8 +112,8 @@ class FileLoader {
 			'base\\gr-main.js',
 			'base\\gr-main-components.js',
 			'base\\gr-lyrics.js',
-			'base\\gr-callbacks.js',
-			'base\\gr-initialize.js'
+			'base\\gr-initialize.js',
+			'base\\gr-callbacks.js'
 		];
 
 		/**
@@ -201,9 +201,9 @@ class Preloader {
 		// * GEOMETRY * //
 		// #region GEOMETRY
 		/** @public @type {number} The global window.Width. */
-		this.ww = 0;
+		this.ww = window.Width;
 		/** @public @type {number} The global window.Height. */
-		this.wh = 0;
+		this.wh = window.Height;
 		/** @public @type {number} The y-position of the lower bar text. */
 		this.lowerBarTextY = 0;
 		/** @public @type {number} The width of the lower bar text. */

profile/georgia-reborn/scripts/Base/gr-helpers.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN             * //
 // * Version:        3.0-RC3                                                 * //
 // * Dev. started:   22-12-2017                                              * //
-// * Last change:    27-10-2024                                              * //
+// * Last change:    30-10-2024                                              * //
 /////////////////////////////////////////////////////////////////////////////////
 
 
@@ -928,13 +928,29 @@ function KeyPressAction(action = {}) {
  * Wraps a synchronous function call that does not return a promise in a promise.
  * This utility function is useful for converting functions that perform synchronous operations
  * into a promise-based interface, allowing them to be used with async/await syntax.
+ * Additionally, it can poll for a condition before resolving.
  * @global
  * @param {function(): void} func - The synchronous function to wrap. This function should not return a promise.
- * @returns {Promise<void>} A promise that resolves immediately after the function call completes.
+ * @param {function(): boolean} [condition] - An optional condition function to check before resolving the promise.
+ * @returns {Promise<void>} A promise that resolves immediately after the function call completes or the condition is met.
  */
-function MakeAsync(func) {
+function MakeAsync(func, condition) {
 	return new Promise(resolve => {
 		func();
+
+		if (condition) {
+			const checkCondition = () => {
+				if (condition()) {
+					clearInterval(interval);
+					resolve();
+				}
+			};
+
+			const interval = setInterval(checkCondition, 50);
+			setTimeout(checkCondition, 0); // Fallback to setTimeout in case the condition is met very quickly
+			return;
+		}
+
 		resolve();
 	});
 }

commit 98851a1dc3e1cb4940f655dbedb1d8befb4c1d84

- Improved initialization and fixed a regression in 4K auto-detection on systemFirstLaunch introduced in commit 14ee427.   * Cleaned up the last callback (on_paint) and moved it to gr-callbacks.js

profile/georgia-reborn/scripts/Base/gr-callbacks.js
@@ -112,6 +112,17 @@ function on_playback_new_track(metadb) {
 
 
 /**
+ * Called when drawing graphics.
+ * Draws the main user interface.
+ * @global
+ * @param {GdiGraphics} gr - The GDI graphics object.
+ */
+function on_paint(gr) {
+	grm.ui.drawMain(gr);
+}
+
+
+/**
  * Called when window is being resized.
  *
  * !IMPORTANT: Do NOT call window.Repaint from this callback!

profile/georgia-reborn/scripts/Base/gr-initialize.js
@@ -6,7 +6,7 @@
 // * Website:        https://github.com/TT-ReBORN/Georgia-ReBORN             * //
 // * Version:        3.0-RC3                                                 * //
 // * Dev. started:   22-12-2017                                              * //
-// * Last change:    27-10-2024                                              * //
+// * Last change:    17-01-2025                                              * //
 /////////////////////////////////////////////////////////////////////////////////
 
 
@@ -61,14 +61,4 @@ if (grSet.systemFirstLaunch) {
 } else {
 	grm.ui.initFonts();
 	grm.ui.initMain();
-}
-
-/**
- * Called when drawing graphics.
- * Draws the main user interface.
- * @global
- * @param {GdiGraphics} gr - The GDI graphics object.
- */
-function on_paint(gr) {
-	grm.ui.drawMain(gr);
 }

profile/georgia-reborn/scripts/Base/gr-main.js
@@ -1935,6 +1935,8 @@ class MainUI {
 		this.setMainComponents('all');
 
 		// * Init panels
+		// Wait for on_size to be loaded in gr-callbacks.js after gr-initialize.js
+		await MakeAsync(() => {}, () => typeof on_size !== 'undefined');
 		await MakeAsync(() => PlaylistRescale(true));
 		await MakeAsync(() => this.initPlaylist());
 		await MakeAsync(() => this.initLibraryPanel());
@@ -1943,8 +1945,6 @@ class MainUI {
 
 		// * Init state
 		if (fb.IsPlaying && fb.GetNowPlaying()) {
-			// Wait for on_playback_new_track to be loaded in gr-callbacks.js
-			await MakeAsync(() => {}, () => typeof on_playback_new_track !== 'undefined');
 			on_playback_new_track(fb.GetNowPlaying());
 		}
 		plman.SetActivePlaylistContext();